2c0c40c83146cb8223df6f9544faa18944fdba47
Remo Lüönd <remo.zumsteg@outlook.com>
2015-06-23 22:17:35 +0800

    added grunt-gump dev-dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,9 +21,29 @@ module.exports = function(grunt){
                 tasks: ['uglify:groupBy']
             }
 
+        },
+
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json', 'bower.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bump');
 };
