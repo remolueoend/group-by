3ca09b0406196cf876dc3f1de8d93b8b7016771b
Remo Lüönd <remo.zumsteg@outlook.com>
2015-06-24 01:04:12 +0800

    enable property pathes

--- src/group-by.js
+++ src/group-by.js
@@ -9,20 +9,49 @@
     if(typeof Array.prototype.groupBy !== 'function'){
 
         /**
+         * Returns the value of the provided property path of an object.
+         * Provided by: http://stackoverflow.com/users/6782/alnitak.
+         * See: http://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
+         *
+         * @param {object} o The object which provides the property.
+         * @param {String} s The property path to access.
+         * @returns {*}
+         * @private
+         */
+        function _resolvePropertyPath(o, s) {
+            s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+            s = s.replace(/^\./, '');           // strip a leading dot
+            var a = s.split('.');
+            for (var i = 0, n = a.length; i < n; ++i) {
+                var k = a[i];
+                if (k in o) {
+                    o = o[k];
+                } else {
+                    return;
+                }
+            }
+            return o;
+        }
+
+        /**
          * Returns a new array grouped by the key returned by keyFn.
          *
-         * @param {function(item)} keyFn Function returning the key to group by.
+         * @param {function(item)|String} key Function returning the key to group by,
+         * or the property string of the key to group by.
          * @returns new GroupedArray instance.
          **/
-        Array.prototype.groupBy = function groupBy(keyFn){
+        Array.prototype.groupBy = function groupBy(key){
             var result = [], _this = this, k;
+            var keyAcc = typeof key === 'function' ? key : function(obj){
+                return _resolvePropertyPath(obj, key);
+            };
             Object.defineProperty(result, 'isGrouped', { value: true });
 
             this.forEach(function(item){
                 if(_this.isGrouped){
                     result.push({key: item.key, items: item.items.groupBy(keyFn)});
                 }else{
-                    k = keyFn(item);
+                    k = keyAcc(item);
                     if(!result.some(function(pair){
                             if(pair.key === k){
                                 pair.items.push(item);
